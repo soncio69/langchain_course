a67e32563dbcdce8ee360aadff427340a13a69eb
soncio69 <soncio69@gmail.com>
2024-05-15 20:00:33 +0200

    nuovi moduli RAG

---
+++ get_dataset.py
@@ -0,0 +1,54 @@
+import time
+import pandas as pd
+from tqdm import tqdm
+from trafilatura.sitemaps import sitemap_search
+from trafilatura import fetch_url, extract, extract_metadata
+
+
+def get_urls_from_sitemap(resource_url: str) -> list:
+    """
+    Funzione che recupera la sitemap attraverso Trafilatura
+    """
+    urls = sitemap_search(resource_url)
+    return urls
+
+
+def create_dataset(list_of_websites: list) -> pd.DataFrame:
+    """
+    Funzione che crea un DataFrame Pandas di URL e articoli.
+    """
+    data = []
+    for website in tqdm(list_of_websites, desc="Websites"):
+        urls = get_urls_from_sitemap(website)
+        for url in tqdm(urls, desc="URLs"):
+            html = fetch_url(url)
+            body = extract(html)
+            try:
+                metadata = extract_metadata(html)
+                title = metadata.title
+                description = metadata.description
+            except:
+                metadata = ""
+                title = ""
+                description = ""
+            d = {
+                'url': url,
+                "body": body,
+                "title": title,
+                "description": description
+            }
+            data.append(d)
+            time.sleep(0.5)
+    df = pd.DataFrame(data)
+    df = df.drop_duplicates()
+    df = df.dropna()
+
+    return df
+
+
+if __name__ == "__main__":
+    list_of_websites = [
+        "https://python.langchain.com/"
+    ]
+    df = create_dataset(list_of_websites)
+    df.to_csv("./data/dataset.csv", index=False)
